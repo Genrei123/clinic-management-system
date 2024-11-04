2176fb47a5c7df21c87fd741e227d3b09751f35f
Xine003 <franxineorias@gmail.com>
2024-11-05 00:09:39 +0800

    Move the BranchController Add Employee Controller/Repo/Service

---
+++ backend/src/main/java/com/jwt/spring_security/model/Employee.java
@@ -0,0 +1,41 @@
+package com.jwt.spring_security.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Employee extends Users {
+
+    private int employeeID;
+
+    private LocalDateTime loginTimeStamp;
+
+    public Employee() {}
+
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public LocalDateTime getLoginTimeStamp() {
+        return loginTimeStamp;
+    }
+
+    public void setLoginTimeStamp(LocalDateTime loginTimeStamp) {
+        this.loginTimeStamp = loginTimeStamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeID=" + employeeID +
+                ", loginTimeStamp=" + loginTimeStamp +
+                ", username='" + getUsername() + '\'' +
+                ", password='" + getPassword() + '\'' +
+                '}';
+    }
+}
