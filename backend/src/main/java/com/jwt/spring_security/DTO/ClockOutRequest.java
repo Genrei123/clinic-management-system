794434a9cc47e56fbb78f95b7afb4cb17d065c81
Lyniel Cris Aya-ay <117515423+Lynlcrz@users.noreply.github.com>
2024-12-19 06:39:30 +0800

    Add files via upload

---
+++ backend/src/main/java/com/jwt/spring_security/DTO/ClockOutRequest.java
@@ -0,0 +1,23 @@
+package com.jwt.spring_security.DTO;
+
+public class ClockOutRequest {
+
+    private String employeeId;
+    private Long branchId;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public Long getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(Long branchId) {
+        this.branchId = branchId;
+    }
+}
